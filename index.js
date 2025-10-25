d08c4dfb272a37a890f85f5b9abb51eab02f411e
zmosDS <122930289+zmosDS@users.noreply.github.com>
2025-10-24 19:49:11 -0700

    updates

---
+++ index.js
@@ -0,0 +1,9 @@
+// Simple script for smooth scrolling between sections
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
